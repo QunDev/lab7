b430b54f895436abea6bb0749981df566951e64e
QunDev <qun942004@gmail.com>
2024-04-07 17:17:56 +0700

    add header accept all conect

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(express.json());
 
